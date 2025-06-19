fe418b63d3378640ba1293667e7a4e2a140fc9e4
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-19 14:14:16 -0600

    08_removeFromArray exercise Progress

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    // for (let i = 0; i < items.length; i++) {
+        if (arr.some(elem => elem === items[i])) {
+            arr.splice(i, 1);
+            items.shift();
+        }
+    // }
+    return arr;
 };
 
 // Do not edit below this line
