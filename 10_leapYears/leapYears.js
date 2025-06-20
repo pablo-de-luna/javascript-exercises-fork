521dd70f331d56f9c49e8a24a4d636d3b57cfbb5
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-20 16:18:58 -0600

    10_leapYears exercise DONE

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+/*
+If the year is divisible by four, it's a leap year.
+But if the year can be divided by 100 as well as four, it's not a leap year.
+However, if the year is divisible by 400, it is a leap year. 
+*/
 
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+    if ((year % 4 === 0) && (year % 100 === 0)) return false;
+    if (year % 4 === 0) return true;
+    return false
 };
 
 // Do not edit below this line
