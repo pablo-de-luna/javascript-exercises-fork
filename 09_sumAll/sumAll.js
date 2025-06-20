77f5bce6cc8eebb36d81e0b74d0a2db54c567172
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-20 13:06:43 -0600

    09_sumAll exercise, Add new solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,21 +1,31 @@
-const sumAll = (...args) => {
-    if (args.some(item => 
-        isNaN(item) 
-        || item < 0
-        || !Number.isInteger(item)
-    )) {
-        return 'ERROR';
-    }
+// const sumAll = (...args) => {
+//     if (args.some(item => item < 0 || !Number.isInteger(item))) {
+//         return 'ERROR';
+//     }
     
-    let firstNum = parseInt(Math.min(...args));
-    let lastNum = parseInt(Math.max(...args));
+//     let firstNum = parseInt(Math.min(...args));
+//     let lastNum = parseInt(Math.max(...args));
+//     let sum = 0;
+    
+//     for (let i = firstNum; i <= lastNum; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// };
+
+const sumAll = (num1, num2) => {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) 
+        return 'ERROR';
+
+    let firstNum = Math.min(num1, num2);
+    let lastNum = Math.max(num1, num2);
     let sum = 0;
     
     for (let i = firstNum; i <= lastNum; i++) {
         sum += i;
     }
     return sum;
-};
+}
 
 
 // Do not edit below this line
