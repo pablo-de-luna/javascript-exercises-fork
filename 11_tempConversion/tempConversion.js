cb89c7fa134b9e0bd739f94b339873d78ecb1551
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-20 17:12:25 -0600

    11_tempConversion exercise DONE

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+/**
+ * From Celsius to Fahrenheit
+ * x °C = (x * (9 / 5) + 32) °F
+ * From Fahrenheit to Celsius
+ * x °F = (x - 32) * (5 / 9) °C
+ */
+
+
+const convertToCelsius = function(value) {
+  resultFromFormula = (value - 32) * (5 / 9);
+  return Math.round(resultFromFormula * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  resultFromFormula = (value * (9 / 5) + 32);
+  return Math.round(resultFromFormula * 10) / 10;
 };
 
 // Do not edit below this line
