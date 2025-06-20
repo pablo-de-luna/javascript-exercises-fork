33f00bfaa89fbbc6386b959fe01a155b349b7385
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-20 12:22:28 -0600

    09_sumAll exercise Progress

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = (...args) => {
+    let firstNum = parseInt(Math.min(...args));
+    let lastNum = parseInt(Math.max(...args));
+    
+    if (firstNum < 0 || lastNum < 0) {
+        return 'ERROR'
+    }
+    
+    console.log('firstNum =', firstNum,'lastNum =', lastNum);
+    
+    let sum = 0;
+    for (let i = firstNum; i <= lastNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
