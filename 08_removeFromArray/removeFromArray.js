3b5bb283981ea1a9e139607af73d6d5aeea654c2
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-06 10:29:19 -0600

    12_calculator started

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,16 +1,20 @@
-// const removeFromArray = (array, ...args) => {
-//     let newArray = [];
-//     array.forEach(item => {
-//         if (!args.includes(item)) {
-//             newArray.push(item);
-//         }
-//     });
-//     return newArray;
-// }
 
-const removeFromArray = (array, ...args) => {
-    return array.filter(item => !args.includes(item));
-}
+// ```javascript
+// removeFromArray([1, 2, 3, 4], 3, 2); // should remove 3 and return [1,2,4]
+// ```
+
+
+const removeFromArray = function(arr, ...items) {
+    let newArr = [];
+
+    arr.forEach(element => {
+        if (!items.some(item => item === element)) {
+            newArr.push(element);
+        }
+    });
+
+    return newArr;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
