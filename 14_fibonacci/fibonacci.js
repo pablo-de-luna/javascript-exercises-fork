a269ed3e55fccf504a4708bebe236d3b5e4e4505
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-06 17:20:32 -0600

    14_fibonacci DONE

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+// fibonacci sequence 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 
+const fibonacci = function(arg) {
+    const seqArr = [1, 1];
+    
+    const index = Number(arg);
+    if (index === 0) return 0;
+    if (index < 0) return "OOPS";
+
+    for (let i = 2; i < index; i++) {
+        seqArr.push(seqArr[seqArr.length - 1] + seqArr[i - 2])
+    }
+
+    return seqArr[seqArr.length - 1];
 };
 
 // Do not edit below this line
