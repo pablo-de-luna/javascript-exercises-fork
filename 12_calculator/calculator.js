d8f494a99e08aa839624e9a50496c95841b76486
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-06 12:19:22 -0600

    12_calculator DONE

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -2,24 +2,30 @@ const add = function(a, b) {
 	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, num) => acc * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (let i = num - 1; i >= 1; i--) {      
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
