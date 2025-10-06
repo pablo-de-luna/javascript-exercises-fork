8d52b10e0e20d8bb5e579a37795900bd5dce0762
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-06 16:12:33 -0600

    13_palindromes DONE

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const filteredStr = string
+        .toLowerCase()
+        .split("")
+        .filter((item) => item.match(/[a-z0-9]/i))
+        .join("");
+    const reversedStr = filteredStr.split("").reverse().join("");
+    return filteredStr === reversedStr;
 };
 
 // Do not edit below this line
