08acee26a1968b3613964a094f9f7e415533a8f1
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-10-07 14:09:17 -0600

    16_findTheOldest DONE

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear()
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    });
+    people.sort((a, b) => b.age - a.age);
+    return people[0];
 };
 
 // Do not edit below this line
