5c6f3a0e19d16f91651f29c3960f64b1f3966c37
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-04-29 20:01:31 -0600

    Complete foundations exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (
+    a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)
+  ) return "ERROR";
 
+  const smallNum = (a < b) ? a : b;
+  const bigNum = (a < b) ? b : a;
+  let sum = 0;
+
+  for (let i = smallNum; i <= bigNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
