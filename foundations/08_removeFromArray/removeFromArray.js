3826039cb32e8bdf4d7e7b6143b3aac05a1f15a9
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-04-29 18:40:26 -0600

    Complete foundations exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+// MY SOLUTION
+// const removeFromArray = function(arr, ...args) {
+//   for (let arg of args) {
+//     for (let i = arr.length; i >= 0; i--) {
+//       if (arr[i] === arg) {
+//         arr.splice(i, 1);
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+// A BETTER WAY
+const removeFromArray = function (arr, ...args) {
+  return arr.filter(elem => !args.includes(elem))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
