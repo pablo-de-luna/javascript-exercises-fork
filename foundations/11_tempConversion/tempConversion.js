eba776fc255703921f5f7fcd7f133db2a65a0a8d
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-04-29 20:59:12 -0600

    Complete foundations exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+//  C to F Formula  °F = °C * 1.8 + 32
+// F to C Formula  °C = (°F - 32) ÷ 1.8
+
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) / 1.8) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * 1.8 + 32) * 10) / 10
 };
 
 // Do not edit below this line
